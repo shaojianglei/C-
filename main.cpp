302093beabd714708590567d31940463e096a50f
邵江磊 <146103441+shaojianglei@users.noreply.github.com>
2023-10-26 15:46:51 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include <stdio.h>
-#include <math.h>
 #include <easyx.h>
+#include <conio.h>
+#include <Windows.h>
+#include <mmsystem.h>
+#include <time.h>
+#pragma comment(lib,"Winmm.lib")
+bool Over;
+bool Win = false;
+bool Lose = false;
+bool exit = false;
+int score = 0;
+void set()
+{
+	Over = false;
+	Win = false;
+	Lose = false;
+	score = 0;//分数
+	srand(time(NULL));//播种
+
+
+
+}
+void drawgame()
+{
+	BeginBatchDraw();
+
+}
 int main(void)
 {
 	return 0;
